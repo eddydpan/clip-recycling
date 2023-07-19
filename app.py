08a806dcb1e27cb5355d26d6c05e1b5e577b24b8
eddydpan <137523226+eddydpan@users.noreply.github.com>
2023-07-19 16:52:59 -0400

    output formatting + included mode of disposal

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ import open_clip
 
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
+
 material_list = []
+disposal_list = []
 
 with open("results.txt", "r") as f:
     results = f.readlines()
     for line in results: 
+        disposal = line.split("\'")[1]
+        disposal_list.append(disposal.strip())
+
         material = line.split(" [")[0]
         material_list.append(material.strip())  # Trim any leading/trailing whitespace
     f.close()
+ 
 text = tokenizer(material_list)
 
 def process_image(image_input, material_input):
@@ -34,11 +40,12 @@ def process_image(image_input, material_input):
     for row in text_probs:
         for column in row:
             float_values.append(float(column))
-            results[float(column)] = material_list[counter] 
+            results[float(column)] = [ material_list[counter], disposal_list[counter] ]
             counter += 1
 
     sorted_float_values = sorted(float_values, reverse=True)
     # print(sorted_float_values)
+
 
     index = -1
     for i in range(len(material_list)):
@@ -48,9 +55,13 @@ def process_image(image_input, material_input):
     if index == -1:
         material_accuracy = None
     else:
-        material_accuracy = material_input + ": " + str(float_values[index])
+        material_accuracy = disposal_list[index] + " - " + material_list[index] + ": " + str(float_values[index])
+    first = results[sorted_float_values[0]][1] + " - " + results[sorted_float_values[0]][0] + ": " + str(sorted_float_values[0])
+    second = results[sorted_float_values[1]][1] + " - " + results[sorted_float_values[1]][0] + ": " + str(sorted_float_values[1])
+    third = results[sorted_float_values[2]][1] + " - " + results[sorted_float_values[2]][0] + ": " + str(sorted_float_values[2])
+    
+    return [first, second, third, material_accuracy]
 
-    return [[results[sorted_float_values[0]], sorted_float_values[0]], [results[sorted_float_values[1]], sorted_float_values[1]], [results[sorted_float_values[2]], sorted_float_values[2]], material_accuracy]
 
 inputs = [gr.inputs.Image(type="pil"), gr.inputs.Dropdown(material_list)]
 outputs = [gr.outputs.Textbox(label="Top Result"), gr.outputs.Textbox(label="Second Result"), gr.outputs.Textbox(label="Third Result"), gr.outputs.Textbox(label="Material Accuracy")]
